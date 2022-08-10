9126c4d834a956332bf913d17f1d193896b3fa3f
PeriodicSeizures <47781580+PeriodicSeizures@users.noreply.github.com>
2022-08-10 01:48:12 -0400

    simple pair object

---
+++ src/main/java/com/crazicrafter1/crutils/Pair.java
@@ -0,0 +1,11 @@
+package com.crazicrafter1.crutils;
+
+public class Pair<A, B> {
+    public A first;
+    public B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+}
